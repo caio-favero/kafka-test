2700b42e3abfcd2d419d7e36ba9adacc3f59bbf7
Caio Favero <caio.favero@ab-inbev.com>
2023-04-03 23:56:30 -0300

    bla

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,27 +1,27 @@
-const { Kafka } = require('kafkajs')
-const topic = 'test-topic'
+const { Kafka } = require("kafkajs");
+const topic = "test-topic";
 
 const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['localhost:9092'],
-})
+  clientId: "my-app",
+  brokers: ["localhost:8080"],
+});
 
-const consumer = kafka.consumer({ groupId: 'test-group' })
+const consumer = kafka.consumer({ groupId: "test-group" });
 
 Promise.resolve(consumer.connect())
-    .then(() => {
-        return consumer.subscribe({ topic, fromBeginning: true })
-    })
-    .then(() => {
-        consumer.subscribe({ topic, fromBeginning: true })
-    })
-    .then(() => {
-        consumer.run({
-            eachMessage: async ({ topic, partition, message }) => {
-                // console.log(message.value.toString())
-            },
-        })
-    })
-    .catch(e => {
-        console.log('err', e)
-    })
+  .then(() => {
+    return consumer.subscribe({ topic, fromBeginning: true });
+  })
+  .then(() => {
+    consumer.subscribe({ topic, fromBeginning: true });
+  })
+  .then(() => {
+    consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        // console.log(message.value.toString())
+      },
+    });
+  })
+  .catch((e) => {
+    console.log("err", e);
+  });
--- consumer/index.js
+++ consumer/index.js
@@ -1,15 +1,16 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs");
 
-const app = require('./config/express')();
-const port = app.get('port');
+const app = require("./config/express")();
+const port = app.get("port");
+console.log(222, app.get("teste"));
 
 const kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka1:9092', 'kafka2:9092'],
-})
+  clientId: "my-app",
+  brokers: ["localhost:8080"],
+});
 
-require('./consumer')
+require("./consumer");
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+  console.log(`Servidor rodando na porta ${port}`);
 });
