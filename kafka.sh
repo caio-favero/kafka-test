bbabf2f9699584b5b5756d172465658ba774d82e
Caio Favero <caio.favero@ab-inbev.com>
2023-04-13 00:27:43 -0300

    kafka version

--- kafka.sh
+++ kafka.sh
@@ -1,8 +1,8 @@
-wget -O kafka.tgz https://downloads.apache.org/kafka/3.4.0/kafka-3.4.0-src.tgz &&
+wget -o kafka.tgz https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz &&
 mkdir kafka &&
 tar zxvf kafka.tgz -C kafka &&
 rm kafka.tgz &&
-cd kafka &&
+cd kafka/kafka_2.13-3.4.0 &&
 KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)" &&
 bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties --ignore-formatted &&
 bin/kafka-server-start.sh config/kraft/server.properties
