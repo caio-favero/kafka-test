a3c0b0e45996d329c988c493093346a1d7320591
Caio Favero <caio.favero@ab-inbev.com>
2023-04-13 09:26:04 -0300

    lushiufs

--- mongoose.js
+++ mongoose.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-main().catch(err => console.log(err))
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test')
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
 
 const kittySchema = new mongoose.Schema({
   _id: String,
   received: Boolean,
-})
+});
 
-const Kitten = mongoose.model('Kitten', kittySchema)
+const Kitten = mongoose.model("Kitten", kittySchema);
 
 // const silence = new Kitten({ _id: 'Silence' })
 // console.log(silence.name) // 'Silence'
 
 module.exports = {
   create: async (_id) => {
-    return await Kitten.create({ _id })
+    return await Kitten.create({ _id });
   },
 
   received: async (_id) => {
-    return await Kitten.findByIdAndUpdate(_id, { received: true })
-  }
-}
+    return await Kitten.findByIdAndUpdate(_id, { received: true });
+  },
+};
 
-// https://www.zabbix.com/download?zabbix=5.0&os_distribution=ubuntu&os_versi
+// https://www.zabbix.com/download?zabbix=6.4&os_distribution=ubuntu&os_version=22.04&components=server_frontend_agent&db=mysql&ws=apache
