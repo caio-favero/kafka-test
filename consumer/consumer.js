f5c21decaab00b834bfa6fd8f6bd0bbddae2a995
Caio Favero <caio.favero@ab-inbev.com>
2023-04-13 00:40:14 -0300

    fix catch error

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,33 +1,51 @@
-const { Kafka } = require("kafkajs")
-const topics = ["test-topic"]
-const mongoose = require('../mongoose')
+const { Kafka } = require("kafkajs");
+const topics = ["test-topic"];
+const mongoose = require("../mongoose");
 
 const kafka = new Kafka({
-  clientId: 'Consumer1',
+  clientId: "Consumer1",
   brokers: ["localhost:9092"],
-})
+});
 
-const consumer = kafka.consumer({ groupId: "test-group1" })
+const consumer = kafka.consumer({ groupId: "test-group1" });
 
 const createConsumer = async () => {
   try {
-    await consumer.connect()
-    await consumer.subscribe({ topics, fromBeginning: true })
+    await consumer.connect();
+    await consumer.subscribe({ topics, fromBeginning: true });
     consumer.run({
       partitionsConsumedConcurrently: 3,
       eachMessage: async ({ topic, partition, message }) => {
-        console.log(topic, partition, JSON.parse(message.value).message)
-        await mongoose.received(JSON.parse(message.value).transactionalId)
+        console.log(topic, partition, JSON.parse(message.value).message);
+        await mongoose.received(JSON.parse(message.value).transactionalId);
       },
-    })
+    });
     consumer.commitOffsets([
-      { topic: topics[0], partition: 0, offset: '31004', high: '31004', low: '421' },
-      { topic: topics[0], partition: 1, offset: '54312', high: '54312', low: '3102' },
-      { topic: topics[0], partition: 2, offset: '32103', high: '32103', low: '518' }
-    ])
+      {
+        topic: topics[0],
+        partition: 0,
+        offset: "31004",
+        high: "31004",
+        low: "421",
+      },
+      {
+        topic: topics[0],
+        partition: 1,
+        offset: "54312",
+        high: "54312",
+        low: "3102",
+      },
+      {
+        topic: topics[0],
+        partition: 2,
+        offset: "32103",
+        high: "32103",
+        low: "518",
+      },
+    ]);
   } catch (error) {
-    console.log('error =>', err)
+    console.log("error =>", error);
   }
-}
+};
 
-createConsumer()
+createConsumer();
--- consumer2/consumer.js
+++ consumer2/consumer.js
@@ -1,29 +1,29 @@
-const { Kafka } = require("kafkajs")
-const topics = ["test-topic", "test-topic2"]
-const mongoose = require('../mongoose')
+const { Kafka } = require("kafkajs");
+const topics = ["test-topic", "test-topic2"];
+const mongoose = require("../mongoose");
 
 const kafka = new Kafka({
-  clientId: 'Consumer2',
+  clientId: "Consumer2",
   brokers: ["localhost:9092"],
-})
+});
 
-const consumer = kafka.consumer({ groupId: "test-group2" })
+const consumer = kafka.consumer({ groupId: "test-group2" });
 
 const createConsumer = async () => {
   try {
-    await consumer.connect()
-    await consumer.subscribe({ topics, fromBeginning: true })
+    await consumer.connect();
+    await consumer.subscribe({ topics, fromBeginning: true });
     consumer.run({
       partitionsConsumedConcurrently: 3,
       autoCommit: false,
       eachMessage: async ({ topic, partition, message }) => {
-        console.log(topic, partition, JSON.parse(message.value).message)
-        await mongoose.received(JSON.parse(message.value).transactionalId)
+        console.log(topic, partition, JSON.parse(message.value).message);
+        await mongoose.received(JSON.parse(message.value).transactionalId);
       },
-    })
+    });
   } catch (error) {
-    console.log('error =>', err)
+    console.log("error =>", error);
   }
-}
+};
 
-createConsumer()
+createConsumer();
